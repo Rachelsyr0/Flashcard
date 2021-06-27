55aa1c9ddd4e3ed8db883bae7c5ce8abef7b1d91
Rachelsyr0 <54992684+Rachelsyr0@users.noreply.github.com>
2021-06-27 20:22:30 +0800

    Update index.js

--- flashcards/basic/scripts/index.js
+++ flashcards/basic/scripts/index.js
@@ -1,27 +1,21 @@
-// Grab the form element from HTML
-var form = document.querySelector("form");
-
-// Grab the pre element from HTML
-var log = document.querySelector("#log");
-
-// Wait for the user to click Submit
-form.addEventListener("submit", function(event) {
-
-  // Retrieve the data from the form
-  var data = new FormData(form);
-  
-  // Create a string to store the data
-  var output = "";
-  
-  // Loop through the data and collect the value of 
-  // the name tag and the value that the user selected
-  for (const entry of data) {
-  
-    // print out the name and its current value
-    output = output + entry[0] + "=" + entry[1] + "\r";
-  };
-  
-  // print out the result
-  log.innerText = output;
-  event.preventDefault();
-}, false);
+let myFlashcards = [ {
+    "question": "120*2",
+    "answer": 240
+    }, 
+    {
+     "question": "30-5",
+     "answer": "25"
+     },
+     {
+     "question": "11*4",
+     "answer": 44
+     }
+     ];
+    
+    
+ let counter = 0;
+ while (counter < myFlashcards.length ) {
+     alert(myFlashcards[counter].question);
+     alert(myFlashcards[counter].answer);
+     ++counter;
+ }
