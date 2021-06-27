293530d74c22f9d58baa5d2f995a2a9b6dced936
Rachelsyr0 <54992684+Rachelsyr0@users.noreply.github.com>
2021-06-27 20:34:54 +0800

    Create index.js

---
+++ flashcards/v2-change/scripts/index.js
@@ -0,0 +1,36 @@
+let myFlashcards = [ {
+    "question": "120*2",
+    "answer": 240
+    }, 
+    {
+     "question": "30-5",
+     "answer": "25"
+     },
+     {
+     "question": "11*4",
+     "answer": 44
+     }
+     ];
+    
+    
+ let counter = 0;
+ let questionAsked = false; // make sure that they ask the question first!
+
+function displayQuestion() {
+     if (counter == myFlashcards.length)
+         counter = 0;
+     document.getElementById("myQuestion").innerHTML = myFlashcards[counter].question;
+     //document.getElementById("myAnswer").innerHTML = "";
+    questionAsked = true;
+ }
+
+function displayAnswer() {
+     if ( questionAsked == true ) {
+        document.getElementById("myAnswer").innerHTML = myFlashcards[counter].answer;
+        questionAsked = false;
+        ++counter;
+     } else {
+        document.getElementById("myAnswer").innerHTML = "Please click on the question first, then the answer.";
+     }
+ }
+
