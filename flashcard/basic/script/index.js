a56ac277bb5f14781b3730c4bed3ed2555a4620e
Rachelsyr0 <54992684+Rachelsyr0@users.noreply.github.com>
2021-07-03 17:05:55 +0800

    Update index.js

--- flashcard/basic/script/index.js
+++ flashcard/basic/script/index.js
@@ -7,15 +7,31 @@ class questionObject {
 
   let flashcard = [];
    
-  // Create three questions and push them onto the quiz array
-  flashcard.push(new questionObject('What is 3+5?', 'more than six?'));
-  flashcard.push(new questionObject('What color is the sky?', 'blue'));
-  flashcard.push(new questionObject('What color is the water?', 'blue'));         
+  // Create questions and push them onto the flashcards array
+  flashcard.push(new questionObject('What is the capital of china?', 'Beijing'));
+  flashcard.push(new questionObject('What is the largest plain in the world?', 'Amazon Plain'));
+  flashcard.push(new questionObject('What is the smallest country in the world?', 'Vatican'));         
   
-  let counter = 0;
-  while (counter < flashcard.length){
-    console.log(flashcard[counter].question);
-    console.log(flashcard[counter].answer);
-    counter += 1 ;
+
+
+  // display the question and answer in order when button are clicked
+  let countQuestion = 0;
+  let countAnswer = 0;
+  document.querySelector('#questionBtn').onclick = function(){
+    if(countQuestion <= countAnswer){
+      alert(flashcard[countQuestion].question);
+      countQuestion += 1;
+    }else{
+       // If the user clicks the next question before clicking the answer
+       alert("Please click on the answer.")
+    }
   }
-    
+  document.querySelector('#answerBtn').onclick = function(){
+    if(countAnswer < countQuestion){
+      alert(flashcard[countAnswer].answer);
+      countAnswer += 1;
+    }else{
+      // If the user clicks the answer before clicking the question
+      alert("You need to click on the question first.")
+    }
+  }
