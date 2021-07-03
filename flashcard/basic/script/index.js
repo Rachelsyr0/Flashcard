177d9376ea17d021d6b6625e133b414353f10c89
Rachelsyr0 <54992684+Rachelsyr0@users.noreply.github.com>
2021-07-03 14:06:41 +0800

    Update index.js

--- flashcard/basic/script/index.js
+++ flashcard/basic/script/index.js
@@ -1,1 +1,22 @@
+const questionObject = {
+  question: 'What is my question?',
+  answer: 'This is my answer',
+}
 
+
+
+
+let flashcard = [];
+ 
+// Create three questions and push them onto the quiz array
+flashcard.push(new questionObject('What is 3+5?', 'more than six?'));
+flashcard.push(new questionObject('What color is the sky?', 'blue'));
+flashcard.push(new questionObject('What color is the water?', 'blue'));         
+
+let counter = 0;
+while (counter < flashcard.length){
+  console.log(flashcard[counter].question);
+  console.log(flashcard[counter].answer);
+  counter += 1 ;
+}
+  
